12f5dc92d07dd09a62dd8c48f00910d07ebd4273
Dan <dan.horne@nexuspoint.co.uk>
2024-04-05 21:04:58 +0100

    Update index.vue

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -1,19 +1,6 @@
 <template>
     <div>
         <StoryblokComponent v-if="story" :blok="story.content" />
-
-        <text-1-col 
-            :content='{
-                "theme": "",
-                "align": "centre",
-                "heading": "For Beauty&apos;s Sake",
-                "subheading": "Discover Brow Perfection: Where Elegance Meets Expertise",
-                "button": {
-                    "text": "Read more",
-                    "url": "about"
-                }
-            }'
-        />
     </div>
 </template>
 
