7adb206ffd290e3189f6f209055afa6dccae8a6e
Dan <dan.horne@nexuspoint.co.uk>
2024-04-07 19:37:12 +0100

    add about page

---
+++ pages/about/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <StoryblokComponent v-if="story" :blok="story.content" />
+    </div>
+</template>
+
+<script setup>
+    const story = await useAsyncStoryblok('about', { version: 'draft' },
+        { 
+        customParent: 'https://app.storyblok.com' 
+        }
+    )
+</script>
