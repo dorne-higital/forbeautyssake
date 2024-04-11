a603f8710cc9ad12eac5be014301468d86b76a90
Dan <dan.horne@nexuspoint.co.uk>
2024-04-11 20:10:22 +0100

    Update PageHero.vue

--- storyblok/Hero/PageHero.vue
+++ storyblok/Hero/PageHero.vue
@@ -6,44 +6,27 @@
         <div 
             class="hero-banner"
             :class="[ 
-                blok.align 
+                blok.theme 
             ]"
         >
-            <div class="hero-content">
-                <p 
-                    v-if="blok?.label?.length"
-                    class="description-header"
-                >
-                    {{ blok.label }}
-                </p>
+            <p 
+                v-if="blok?.label?.length"
+                class="description-header"
+            >
+                {{ blok.label }}
+            </p>
 
-                <h1
-                    v-if="blok?.heading?.length"
-                >
-                    {{ blok.heading }}
-                </h1>
+            <h1
+                v-if="blok?.heading?.length"
+            >
+                {{ blok.heading }}
+            </h1>
 
-                <h3
-                    v-if="blok?.subheading?.length"
-                >
-                    {{ blok.subheading }}
-                </h3>
-
-                <nuxt-link 
-                    v-if="blok?.linkText?.length"
-                    :to="blok.linkUrl.url"
-                    class="button"
-                >
-                    {{ blok.linkText }}
-                </nuxt-link>
-            </div>
-
-            <div class="img-container">
-                <img 
-                    :src="blok.image.filename" 
-                    :alt="blok.image.alt"
-                >
-            </div>
+            <h3
+                v-if="blok?.subheading?.length"
+            >
+                {{ blok.subheading }}
+            </h3>
         </div>
 
         <div class="hero-shape"></div>
@@ -70,103 +53,20 @@
     .hero-container {
     }
 
-    //////////////////
-    // Align styles //
-    //////////////////
-    
-    .imgRight {
-        flex-direction: row;
-
-        .img-container {
-            clip-path: polygon(15% 0, 100% 0, 100% 100%, 15% 100%, 0 50%);
-        }
-    }
-
-    .imgLeft {
-        flex-direction: row-reverse;
-
-        .img-container {
-            clip-path: polygon(0 0, 85% 0, 100% 50%, 85% 100%, 0 100%);
-        }
-    }
-
     .hero-banner {
         align-items: center;
+        background: $color-1;
+        clip-path: polygon(0 0, 100% 0, 100% 80%, 50% 100%, 0 80%);
         color: $primary-color;
         display: flex;
-        height: 20rem;
+        flex-direction: column;
         justify-content: center;
-        padding: 0 0 5rem;
+        padding: 4rem;
 
         @media (max-width: 600px) {
             flex-wrap: wrap;
             gap: 1rem;
             padding: 1rem;
-        }
-
-        .hero-content {
-            background: linear-gradient(90deg, 
-                transparent 40%, 
-                rgba(247,157,132,1) 40%, 
-                rgba(247,157,132,1) 85%, 
-                transparent 85%);
-            display: flex;
-            flex: 1;
-            flex-direction: column;
-            justify-content: center;
-            padding: 0 5rem;
-            text-align: left;
-            width: 50%;
-
-            @media (max-width: 600px) {
-                background: none;
-                padding: 7rem 1rem 3rem;
-                width: 100%;
-            }
-
-            .description-header {
-                color: $color-2;
-                font-weight: 500;
-            }
-
-            .button {
-                background-color: $color-2;
-                border: 2px solid $color-2;
-                color: $secondary-color;
-                cursor: pointer;
-                font-size: 1rem;
-                margin: .5rem 0;
-                padding: .75rem 2.25rem;
-                width: fit-content;
-
-                &:hover {
-                    border: 2px solid #175c53;
-                    color: $primary-color;
-                }
-            }
-        }
-
-        .img-container {
-            display: flex;
-            flex: 1;
-            flex-wrap: wrap;
-            height: 100%;
-            width: 50%;
-
-            @media (max-width: 600px) {
-                padding: 0 1rem;
-            }
-
-            img {
-                height: 100%;
-                width: 100%;
-                object-fit: cover;
-                overflow: hidden;
-
-                @media (max-width: 600px) {
-                    margin-bottom: 2rem;
-                }
-            }
         }
     }  
 </style>
