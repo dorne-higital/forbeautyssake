205d8299edeaa0838ae02f4e20d83998e28dfff7
Dan <dan.horne@nexuspoint.co.uk>
2024-04-15 22:36:57 +0100

    add nav item blok

---
+++ storyblok/Layouts/NavItem.vue
@@ -0,0 +1,21 @@
+<template>
+    <li
+        v-editable="blok"
+        class="nav-item"
+    >
+        <a :href="blok.LinkUrl.url">
+            {{ blok.LinkTitle }}
+        </a>
+    </li>
+</template>
+
+<script setup>
+    defineProps({ blok: Object });
+</script>
+
+
+
+<style lang="scss" scoped>
+    @import "~/assets/css/main.scss";
+
+</style>
