4a9e6e9c185d29cb24bb557aea9269ec9108e8f6
Dan <dan.horne@nexuspoint.co.uk>
2024-04-15 22:22:40 +0100

    Update Page.vue

--- storyblok/Page.vue
+++ storyblok/Page.vue
@@ -1,6 +1,22 @@
 <template>
   <div v-editable="blok" class="px-4">
-    <StoryblokComponent v-for="blok in blok.body" :key="blok._uid" :blok="blok" />
+    <StoryblokComponent 
+      v-for="blok in blok.nav" 
+      :key="blok._uid" 
+      :blok="blok" 
+    />
+
+    <StoryblokComponent 
+      v-for="blok in blok.body" 
+      :key="blok._uid" 
+      :blok="blok" 
+    />
+    
+    <StoryblokComponent 
+      v-for="blok in blok.footer" 
+      :key="blok._uid" 
+      :blok="blok" 
+    />
   </div>
 </template>
 
