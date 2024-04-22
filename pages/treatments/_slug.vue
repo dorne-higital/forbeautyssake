d6e21ba1b1c311b505bf7d7c134178cdfedc2604
Dan <hornedigital01@gmail.com>
2024-04-22 20:40:58 +0100

    push up

---
+++ pages/treatments/_slug.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>{{ treatment.title }}</h1>
+
+        <div v-html="treatment.content"></div>
+    </div>
+</template>
+  
+<script>
+    export default {
+        async asyncData({ $storyapi, params }) {
+            const { data } = await $storyapi.get(`cdn/stories/treatments/${params.slug}`);
+            
+            return {
+                treatment: {
+                    title: data.story.content.title,
+                    content: data.story.content.body, // Assuming the body field contains the treatment content
+                },
+            };
+        },
+    };
+</script>
+  
