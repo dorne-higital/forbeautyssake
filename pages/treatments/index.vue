c59237fc2d33d92dcfc54ecb76a5f7ba958b97db
Dan <hornedigital01@gmail.com>
2024-04-22 20:44:20 +0100

    update

--- pages/treatments/index.vue
+++ pages/treatments/index.vue
@@ -1,33 +1,13 @@
 <template>
     <div>
-        <ul>
-            <li v-for="treatment in treatments" :key="treatment.slug">
-                <nuxt-link :to="`/treatment/${treatment.slug}`">{{ treatment.title }}</nuxt-link>
-            </li>
-        </ul>
         <StoryblokComponent v-if="story" :blok="story.content" />
     </div>
 </template>
 
-<script>
-    // const story = await useAsyncStoryblok('treatments', { version: 'draft' },
-    //     { 
-    //     customParent: 'https://app.storyblok.com' 
-    //     }
-    // )
-
-    export default {
-        async asyncData({ $storyapi }) {
-            const { data } = await $storyapi.get('cdn/stories', {
-                starts_with: 'treatment/',
-                sort_by: 'content.title:asc',
-            });
-            return {
-                treatments: data.stories.map((story) => ({
-                    title: story.content.title,
-                    slug: story.slug,
-                })),
-            };
-        },
-    };
+<script setup>
+    const story = await useAsyncStoryblok('treatments', { version: 'draft' },
+        { 
+        customParent: 'https://app.storyblok.com' 
+        }
+    )
 </script>
