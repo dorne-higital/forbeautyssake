cfe803921a21ef25e3df22c6b92c562a76c4a1f6
Dan <hornedigital01@gmail.com>
2024-05-01 19:53:10 +0100

    Update CallToActionBanner.vue

--- storyblok/Content/CallToActionBanner.vue
+++ storyblok/Content/CallToActionBanner.vue
@@ -12,19 +12,19 @@
         <div 
             class="content-container"
         >
-            <h3
+            <h2
                 v-if="blok?.subheading?.length"
                 class="subheading"
             >
                 {{ blok.heading }}
-            </h3>
+            </h2>
 
-            <p
-                v-if="blok?.description?.length"
+            <h4
+                v-if="blok?.subheading?.length"
                 class="description"
             >
                 {{ blok.subheading }}
-            </p>
+        </h4>
         </div>
 
         <div class="button-container">
@@ -58,10 +58,11 @@
     @import "~/assets/css/main.scss";
 
     .cta-banner {
+        align-items: center;
         display: flex;
         flex-direction: row;
         gap: 2rem;
-        padding: 5rem 2rem;
+        padding: 5rem 15rem;
         width: 100%;
 
         @media (max-width: 600px) {
@@ -76,13 +77,17 @@
             background-color: $bg-secondary;
             color: $secondary-color;
 
-            .content-container .button {
-                background-color: $bg-primary;
-                border: 2px solid $bg-primary;
-                color: $primary-color;
+            .button-container {
+                .button {
+                    background: $bg-primary;
+                    border: 2px solid $color-2;
+                    box-shadow: .5rem .5rem 0 0 $color-2;
+                    color: $primary-color;
 
-                &:hover {
-                    color: $secondary-color;
+                    &:hover {
+                        border: 2px solid $color-1;
+                        box-shadow: .5rem .5rem 0 0 $color-1;
+                    }
                 }
             }
         }
@@ -90,15 +95,63 @@
         &.color-1 {
             background-color: $color-1;
             color: $primary-color;
+
+            .button-container {
+                .button {
+                    background: $bg-primary;
+                    border: 2px solid $bg-primary;
+                    box-shadow: .5rem .5rem 0 0 $bg-secondary;
+                    color: $primary-color;
+
+                    &:hover {
+                        border: 2px solid $color-2;
+                        box-shadow: .5rem .5rem 0 0 $color-2;
+                    }
+                }
+            }
         }
 
         &.color-2 {
             background-color: $color-2;
             color: $primary-color;
+
+            .button-container {
+                .button {
+                    background: $bg-primary;
+                    border: 2px solid $bg-primary;
+                    box-shadow: .5rem .5rem 0 0 $bg-secondary;
+                    color: $primary-color;
+
+                    &:hover {
+                        border: 2px solid $color-1;
+                        box-shadow: .5rem .5rem 0 0 $color-1;
+                    }
+                }
+            }
         }
 
         .content-container {
+            width: 70%;
+        }
 
+        .button-container {
+            .button {
+                background: $color-1;
+                border: 2px solid $color-1;
+                box-shadow: .5rem .5rem 0 0 $color-2;
+                color: $primary-color;
+                cursor: pointer;
+                font-size: 1.2rem;
+                font-weight: 400;
+                padding: .7rem 2rem;
+                transition: .5s;
+
+                &:hover {
+                    background: $bg-primary;
+                    box-shadow: .5rem .5rem 0 0 $color-1;
+                    transition: .5s;
+                }
+            }
         }
     }
 </style>
