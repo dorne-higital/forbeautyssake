8b1cae6a2cc90a1d18d207d1a8ead9bd7af18984
Dan <hornedigital01@gmail.com>
2024-08-12 20:23:39 +0100

    contact page

---
+++ storyblok/Content/ContactCol2.vue
@@ -0,0 +1,90 @@
+<template>
+    <section
+        v-editable="blok"
+        :style="{ 
+            backgroundColor: blok.bgColour.value 
+        }"
+        class="image-text"
+    >
+		<div class="content-container">
+			<div class="opening-hours">
+				<OpeningHours :blok="story.content.openingHours" />
+			</div>
+
+			<div class="social-links">
+
+			</div>
+		</div>
+
+		<!-- Map -->
+        <div class="img-container">
+            <img 
+                :src="blok.image.filename" 
+                :alt="blok.image.alt"
+            >
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: 'contactCol2',
+        props: {
+            componentName: {
+                type: String,
+                default: 'contact-col-2'
+            },
+            blok: {
+                type: String,
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    @import "~/assets/css/main.scss";
+
+    .contact-col-2 {
+        display: flex;
+        flex-direction: row;
+        gap: 2rem;
+        padding: 5rem 2rem;
+        width: 100%;
+
+        @media (max-width: 600px) {
+            flex-direction: column-reverse;
+            padding: 2rem 1rem;
+            width: 100%;
+        }
+
+        .content-container {
+            flex: 1;
+            padding: 1rem 2rem;
+            width: 50%;
+
+            @media (max-width: 600px) {
+                padding: 1rem;
+                width: 100%;
+            }
+        }
+
+        .img-container {
+            flex: 1;
+            width: 50%;
+
+            @media (max-width: 600px) {
+                width: 100%;
+            }
+
+            img {
+                max-height: 30rem;
+                width: 100%;
+                object-fit: cover;
+
+                @media (max-width: 600px) {
+                    max-height: 20rem;
+                }
+            }
+        }
+    }
+</style>
---
+++ storyblok/Content/OpeningHours.vue
@@ -0,0 +1,38 @@
+<template>
+    <section
+        v-editable="blok"
+        :style="{ 
+            backgroundColor: blok.bgColour.value 
+        }"
+        class="opening-hours"
+    >
+		<ul>
+			<li v-for="(hour, index) in blok.openingHours" :key="index">
+				<strong>{{ hour.day }}</strong>: {{ hour.openingTime }} - {{ hour.closingTime }}
+			</li>
+		</ul>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: 'openingHours',
+        props: {
+            componentName: {
+                type: String,
+                default: 'opening-hours'
+            },
+            blok: {
+                type: String,
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    @import "~/assets/css/main.scss";
+
+    .opening-hours {
+
+    }
+</style>
