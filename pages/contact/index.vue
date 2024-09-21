d40a2d3013d8409898bdc05d746f4c1524168f7a
Dan <hornedigital01@gmail.com>
2024-09-21 21:01:28 +0100

    Update index.vue

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -1,8 +1,7 @@
 <template>
     <div>
         <StoryblokComponent v-if="story" :blok="story.content" />
-
-        <!-- <iframe src="https://widget.tagembed.com/158934?view" style=" width:100%;height:500px;overflow: auto;" frameborder="0" allowtransparency="true"></iframe>    </div> -->
+    </div>
 </template>
 
 <script setup>
