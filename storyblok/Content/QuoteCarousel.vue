02fb6ab6305b455750a51ba27b18d5739d1a8c50
Dan <hornedigital01@gmail.com>
2024-09-22 13:20:26 +0100

    new quote build

---
+++ storyblok/Content/QuoteCarousel.vue
@@ -0,0 +1,68 @@
+<template>
+    <section
+        v-editable="blok"
+        :style="{ 
+            backgroundColor: blok.bgColour.value 
+        }"
+        class="quote-carousel"
+    >
+        <div class="content-container">
+            <p 
+                v-if="blok?.label?.length"
+                class="description-header"
+            >
+                {{ blok.label }}
+            </p>
+
+            <h2 v-if="blok?.heading?.length">
+                {{ blok.heading }}
+            </h2>
+
+            <h4 v-if="blok?.subheading?.length">
+                {{ blok.subheading }}
+            </h4>
+        </div>
+
+		<StoryblokComponent
+			v-for="blok in blok.quotes"
+			:key="blok._uid"
+			:blok="blok"
+		/>
+
+    </section>
+</template>
+
+<script>
+    export default {
+        name: 'quoteCarousel',
+        components: {
+        },
+        props: {
+            componentName: {
+                type: String,
+                default: 'quote-carousel'
+            },
+            blok: {
+                type: String,
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    @import "~/assets/css/main.scss";
+
+    .quote-carousel {
+        display: flex;
+        flex-direction: row;
+        gap: 2rem;
+        padding: 5rem 2rem;
+        width: 100%;
+
+        @media (max-width: 600px) {
+            flex-direction: column;
+            padding: 2rem 1rem;
+            width: 100%;
+        }
+    }
+</style>
---
+++ storyblok/Content/Quotes.vue
@@ -0,0 +1,55 @@
+<template>
+    <section
+        v-editable="blok"
+        :style="{ 
+            backgroundColor: blok.bgColour.value 
+        }"
+        class="quotes"
+    >
+        <div class="content-container">
+            <h4 v-if="blok?.quote?.length">
+                {{ blok.quote }}
+			</h4>
+
+            <p v-if="blok?.author?.length">
+                {{ blok.author }}
+			</p>
+        </div>
+
+    </section>
+</template>
+
+<script>
+    export default {
+        name: 'quotes',
+        components: {
+        },
+        props: {
+            componentName: {
+                type: String,
+                default: 'quotes'
+            },
+            blok: {
+                type: String,
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    @import "~/assets/css/main.scss";
+
+    .quotes {
+        display: flex;
+        flex-direction: row;
+        gap: 2rem;
+        padding: 5rem 2rem;
+        width: 100%;
+
+        @media (max-width: 600px) {
+            flex-direction: column;
+            padding: 2rem 1rem;
+            width: 100%;
+        }
+    }
+</style>
