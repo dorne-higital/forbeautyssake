fc658f81c9828622eb9cfd083d2c768f820baa9c
Dan <hornedigital01@gmail.com>
2024-09-22 15:05:54 +0100

    Create index.vue

---
+++ pages/terms-and-conditions/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <StoryblokComponent v-if="story" :blok="story.content" />
+    </div>
+</template>
+
+<script setup>
+    const story = await useAsyncStoryblok('terms-and-conditions', { version: 'draft' },
+        { 
+        customParent: 'https://app.storyblok.com' 
+        }
+    )
+</script>
