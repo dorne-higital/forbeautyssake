5995d59bac7cae339715a51ad00a53ccd65bdaa7
Dan <hornedigital01@gmail.com>
2024-10-22 11:04:40 +0100

    Create index.vue

---
+++ pages/nails-by-charlotte/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <StoryblokComponent v-if="story" :blok="story.content" />
+    </div>
+</template>
+
+<script setup>
+    const story = await useAsyncStoryblok('nails-by-charlotte', { version: 'draft' },
+        { 
+        customParent: 'https://app.storyblok.com' 
+        }
+    )
+
+    useHead({
+        title: 'ForbeautysSake | Nails by Charlottoe',
+        meta: [
+            {
+                name: 'description',
+                content: 'The best way to reach us is via social media, through Facebook or Instagram. Alternatively, please feel free to email us, or pop in and speak directly with us.'
+            },
+            {
+                property: 'og:title',
+                content: 'ForbeautysSake | Nails by Charlotte'
+            },
+            {
+                property: 'og:description',
+                content: 'The best way to reach us is via social media, through Facebook or Instagram. Alternatively, please feel free to email us, or pop in and speak directly with us.'
+            },
+        ]
+    })
+</script>
