f3bf177c2291cd1f8aedd93842ca5338becc2968
Dan <hornedigital01@gmail.com>
2024-11-11 21:16:53 +0000

    add stat comp

---
+++ storyblok/Content/StatCard.vue
@@ -0,0 +1,61 @@
+<template>
+    <section
+        v-editable="blok"
+        class="stat-card"
+    >
+        <div class="content-container">
+            <h4 v-if="blok?.quote?.length">
+                {{ blok.stat }}
+			</h4>
+
+            <p 
+				v-if="blok?.author?.length"
+				class="stat-label"
+			>
+                {{ blok.label }}
+			</p>
+        </div>
+
+    </section>
+</template>
+
+<script>
+    export default {
+        name: 'statCard',
+        components: {
+        },
+        props: {
+            componentName: {
+                type: String,
+                default: 'stat-card'
+            },
+            blok: {
+                type: Object,
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    @import "~/assets/css/main.scss";
+
+    .stat-card {
+        display: flex;
+        flex-direction: row;
+        gap: 2rem;
+        padding: 2rem;
+        width: 25%;
+
+        @media (max-width: 600px) {
+            flex-direction: column;
+            padding: 1rem;
+            width: 50%;
+        }
+
+		.content-container {
+			margin: 0 auto;
+			text-align: center;
+			width: 80%;
+		}
+    }
+</style>
---
+++ storyblok/Grids/StatGrid.vue
@@ -0,0 +1,84 @@
+<template>
+    <section
+        v-editable="blok"
+        :style="{ 
+            backgroundColor: blok.bgColour.value 
+        }"
+        class="stat-grid"
+    >
+        <div class="block-container">
+            <StoryblokComponent
+                v-for="card in blok.stats"
+                :key="card._uid"
+                :blok="card"
+            />
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: 'statGrid',
+        componentName: {
+            type: String,
+            default: 'stat-grid'
+        },
+        props: {
+            blok: {
+                type: Object
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+    @import "~/assets/css/main.scss";
+
+    .stat-grid {
+		align-items: center;
+        background: $bg-tertiary;
+        color: $tertiary-color;
+        display: flex;
+        flex-direction: column;
+        padding: 5rem;
+        width: 100%;
+
+        @media (max-width: 600px) {
+            padding: 2rem 1rem;
+            width: 100%;
+        }
+
+        //////////////////
+        // Theme styles //
+        //////////////////
+        
+        &.theme-dark {
+            background-color: $bg-secondary;
+            color: $secondary-color;
+        }
+
+        &.theme-color-1 {
+            background-color: $color-1;
+            color: $primary-color;
+        }
+
+        &.theme-color-2 {
+            background-color: $color-2;
+            color: $secondary-color;
+        }
+
+        .block-container {
+            display: flex;
+            flex-direction: row;
+            flex-wrap: wrap;
+            padding: 1rem;
+			width: 100%;
+            max-width: $sw;
+
+            @media (max-width: 600px) {
+                padding: 0;
+                width: 100%;
+            }
+        }
+    }
+</style>
